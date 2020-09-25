ab87515269cc7a71347abbc0a13f7b2b9a3732d3
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2020-09-25 11:18:39 +0500

    Created AssertUtils

--- utils/src/main/java/kz/q19/utils/assert/AssertUtils.kt
+++ utils/src/main/java/kz/q19/utils/assert/AssertUtils.kt
@@ -1,10 +1,17 @@
+@file:Suppress("unused")
+
 package kz.q19.utils.assert
 
-/**
- * Helper method which throws an exception when an assertion has failed
- */
-fun assertIsTrue(condition: Boolean) {
-    if (!condition) {
-        throw AssertionError("Expected condition to be true")
+object AssertUtils {
+
+    /**
+     * Helper method which throws an exception when an assertion has failed
+     */
+    @JvmStatic
+    fun assertIsTrue(condition: Boolean) {
+        if (!condition) {
+            throw AssertionError("Expected condition to be true")
+        }
     }
+
 }
