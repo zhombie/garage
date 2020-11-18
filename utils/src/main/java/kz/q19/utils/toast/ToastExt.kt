5314004e7aa2e66b14ec649fa9bb8ed70e3c6cd5
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2020-11-19 03:32:00 +0500

    Return Toast after show

--- utils/src/main/java/kz/q19/utils/toast/ToastExt.kt
+++ utils/src/main/java/kz/q19/utils/toast/ToastExt.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import android.widget.Toast
 import androidx.annotation.StringRes
 
-fun Context?.toast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
-    this?.let { Toast.makeText(it, text, duration).show() }
+fun Context.toast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT): Toast {
+    val toast = Toast.makeText(this, text, duration)
+    toast.show()
+    return toast
 }
 
-fun Context?.toast(@StringRes textId: Int, duration: Int = Toast.LENGTH_SHORT) {
-    this?.let { Toast.makeText(it, textId, duration).show() }
+fun Context.toast(@StringRes textId: Int, duration: Int = Toast.LENGTH_SHORT): Toast {
+    val toast = Toast.makeText(this, textId, duration)
+    toast.show()
+    return toast
 }
--- utils/src/main/java/kz/q19/utils/view/binding/ViewBinding.kt
+++ utils/src/main/java/kz/q19/utils/view/binding/ViewBinding.kt
@@ -9,10 +9,10 @@ fun <T> lazyUnsynchronized(initializer: () -> T): Lazy<T> =
     lazy(LazyThreadSafetyMode.NONE, initializer)
 
 fun <T : View> Activity.bind(@IdRes res: Int): Lazy<T> =
-    lazyUnsynchronized { findViewById<T>(res) }
+    lazyUnsynchronized { findViewById(res) }
 
 fun <T : View> View.bind(@IdRes res: Int): Lazy<T> =
-    lazyUnsynchronized { findViewById<T>(res) }
+    lazyUnsynchronized { findViewById(res) }
 
 fun <T : View> RecyclerView.ViewHolder.bind(@IdRes idRes: Int): Lazy<T> =
-    lazyUnsynchronized { itemView.findViewById<T>(idRes) }
+    lazyUnsynchronized { itemView.findViewById(idRes) }
