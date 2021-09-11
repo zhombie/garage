8198930e7ac0c235c29db663d5761cf926c6f783
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2021-09-12 00:10:40 +0600

    [Drawable] Ext for tint

--- utils/src/main/java/kz/q19/utils/view/binding/ViewBinding.kt
+++ utils/src/main/java/kz/q19/utils/view/binding/ViewBinding.kt
@@ -8,11 +8,11 @@ import androidx.recyclerview.widget.RecyclerView
 fun <T> lazyUnsynchronized(initializer: () -> T): Lazy<T> =
     lazy(LazyThreadSafetyMode.NONE, initializer)
 
-fun <T : View> Activity.bind(@IdRes res: Int): Lazy<T> =
-    lazyUnsynchronized { findViewById(res) }
+fun <T : View> Activity.bind(@IdRes id: Int): Lazy<T> =
+    lazyUnsynchronized { findViewById(id) }
 
-fun <T : View> View.bind(@IdRes res: Int): Lazy<T> =
-    lazyUnsynchronized { findViewById(res) }
+fun <T : View> View.bind(@IdRes id: Int): Lazy<T> =
+    lazyUnsynchronized { findViewById(id) }
 
-fun <T : View> RecyclerView.ViewHolder.bind(@IdRes idRes: Int): Lazy<T> =
-    lazyUnsynchronized { itemView.findViewById(idRes) }
+fun <T : View> RecyclerView.ViewHolder.bind(@IdRes id: Int): Lazy<T> =
+    lazyUnsynchronized { itemView.findViewById(id) }
