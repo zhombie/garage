2fd726eb811ec1fe8b4b174641b4b0b803767431
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2021-11-22 14:30:28 +0600

    Suppress deprecation for compat functions

--- text/src/main/java/kz/q19/utils/text/HTMLCompat.kt
+++ text/src/main/java/kz/q19/utils/text/HTMLCompat.kt
@@ -9,6 +9,7 @@ object HTMLCompat {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             Html.fromHtml(text, Html.FROM_HTML_MODE_COMPACT)
         } else {
+            @Suppress("DEPRECATION")
             Html.fromHtml(text)
         }
     }
--- window/src/main/java/kz/q19/utils/window/DisplayExt.kt
+++ window/src/main/java/kz/q19/utils/window/DisplayExt.kt
@@ -10,6 +10,7 @@ fun Context.getDisplayCompat(): Display? {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
         display
     } else {
+        @Suppress("DEPRECATION")
         ContextCompat.getSystemService(this, WindowManager::class.java)?.defaultDisplay
     }
 }
