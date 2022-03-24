6b8bd1a72b24f23e0595d5ba0a9e1359cc375ca7
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2022-03-24 17:04:04 +0600

    [multimedia-store] Folder secondary constructor

--- multimedia/store/src/main/java/kz/garage/multimedia/store/model/Folder.kt
+++ multimedia/store/src/main/java/kz/garage/multimedia/store/model/Folder.kt
@@ -3,6 +3,7 @@ package kz.garage.multimedia.store.model
 import android.net.Uri
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlin.random.Random
 
 @Parcelize
 data class Folder constructor(
@@ -10,6 +11,16 @@ data class Folder constructor(
     val displayName: String? = null,
     val items: List<Content> = emptyList()
 ) : Parcelable {
+
+    companion object {
+        fun generateId(): Long =
+            System.currentTimeMillis() + Random.nextLong(Long.MIN_VALUE, Long.MAX_VALUE)
+    }
+
+    constructor(
+        displayName: String?,
+        items: List<Content>
+    ) : this(id = generateId(), displayName = displayName, items = items)
 
     val cover: Uri?
         get() = if (items.isEmpty()) null else items.first().uri
